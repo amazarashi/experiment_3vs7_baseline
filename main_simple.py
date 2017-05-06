cb3072af259cdec29f7dcc382c37122ba9b546ef
suguru <u.sgr.22114w@gmail.com>
2017-05-06 17:12:04 +0900

    bug fiexd

--- main_simple.py
+++ main_simple.py
@@ -40,6 +40,6 @@ if __name__ == '__main__':
     args['dataset'] = dataset
     args['dataaugumentation'] = dataaugumentation
     args['elseIndices'] = elseIndices
-    print(args.keys)
+    print(args.keys())
     main = amaz_trainer_batchInbatch.Trainer(**args)
     main.run()
