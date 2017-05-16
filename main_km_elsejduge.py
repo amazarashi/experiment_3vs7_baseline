a5a7ceefa215cbceb2909ad9f50c42454b906788
suguru <u.sgr.22114w@gmail.com>
2017-05-16 14:29:03 +0900

    preparing kmenas process

--- main_km_elsejduge.py
+++ main_km_elsejduge.py
@@ -80,8 +80,8 @@ if __name__ == "__main__":
                 print(labelname,":",maxdis)
                 distance = amaz_kmeans.KmeansProcess().calc_distance_2point(centroid,feature)
                 print("distances:",distance)
-                print(centroid)
-                print(feature)
+                print(centroid[:20])
+                print(feature[:20])
                 if distance < maxdis:
                     elseStatus = True
                     nonelse_judge += 1
