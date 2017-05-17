1eb793782a5ccf55a9c6113e376768890539fd2a
suguru <u.sgr.22114w@gmail.com>
2017-05-17 19:13:40 +0900

    debug

--- amaz_kmeans.py
+++ amaz_kmeans.py
@@ -79,8 +79,8 @@ class KmeansProcess(object):
                 xin = datashaping.prepareinput(da_x,dtype=np.float32,volatile=True)
                 xin.to_gpu()
                 feature = model.getFeature(xin,train=False) #(batch,1024)
-                #feature.to_cpu()
-                #[features.append(f.data) for f in feature]
+                feature.to_cpu()
+                [features.append(f.data) for f in feature]
                 print(feature.shape)
                 print(i)
             centroid,maxdis,mindis = self.calc_categorical_centroid(np.array(features))
