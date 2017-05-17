16e16f2ae615963b74e7984a9dd339aa226e3d9b
suguru <u.sgr.22114w@gmail.com>
2017-05-18 00:52:42 +0900

    debug

--- darknet19.py
+++ darknet19.py
@@ -128,7 +128,7 @@ class Darknet19(chainer.Chain):
 
     def calc_kmeansloss(self,y,t,km_features,epoch,centroids,volatile=False):
         #label loss
-        batch,_ y.shape
+        batch,_ = y.shape
         tt = np.zeros(y.shape,dtype=np.float32)
         for i,tind in enumerate(t):
             tt[i][int(tind.data)] = 1
